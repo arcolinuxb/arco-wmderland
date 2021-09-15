9cc688129785413be0ea7f62e60edd27b2d7d536
Erik Dubois <erik.dubois@gmail.com>
2021-09-15 10:29:20 +0200

    changed to wmderland

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -26,8 +26,8 @@ echo
 	#Let us set the desktop"
 	#First letter of desktop is small letter
 
-	desktop="bspwm"
-	dmDesktop="bspwm"
+	desktop="wmderland"
+	dmDesktop="wmderland"
 
 	arcolinuxVersion='v21.11.1'
 
