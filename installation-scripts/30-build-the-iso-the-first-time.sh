822ae7a29f4a521e32153a7ce70a74749f6278c2
Erik Dubois <erik.dubois@gmail.com>
2021-09-24 19:00:27 +0200

    new calamare

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="wmderland"
 	dmDesktop="wmderland"
 
-	arcolinuxVersion='v21.11.02'
+	arcolinuxVersion='v21.11.03'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
